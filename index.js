5241c635ca1b35f77b77bb379a0fca66b4f9a8be
rifat028 <istiakrifat001@gmail.com>
2025-11-13 19:00:18 +0600

    insert one boook implemented

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// fN5nJSlPuhbTzQZu
+// TheBookHeaven
+const uri =
+  "mongodb+srv://TheBookHeaven:fN5nJSlPuhbTzQZu@module54.p4tcocf.mongodb.net/?appName=Module54";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+app.get("/", (req, res) => {
+  res.send("The Book Heaven Server Running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("bookDB");
+    const bookCollection = database.collection("books");
+
+    // Single Book insert
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Your deployment successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`The Book Heaven Server Listening on port ${port}`);
 });
